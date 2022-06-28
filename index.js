43e84c9d50500f814de3e5f67c6e210fdec5e252
Brennen Shtern <brennen.shtern@gmail.com>
2022-06-28 13:33:36 -0400

    commit tree 90baee0ef17edc75c5be51ca63011516cfce34f9

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    return cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+    return cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift("Milo");
+}
+function appendCat(Broom){
+    return appendCat = [...cats, "Broom"];
+}
+function prependCat(Arnold){
+    return prependCat = ["Arnold", ...cats];
+}
+function removeLastCat(){
+    return removeLastCat = [...cats.slice(0,-1)];
+}
+function removeFirstCat(){
+    return removeFirstCat = [...cats.slice(1)];
+}
